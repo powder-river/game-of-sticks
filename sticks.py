b27827bd31776b88d03382a1a92e200928be4ee4
Danai Adkisson <dadkisso@live.com>
2015-09-08 13:56:05 -0400

    wrote and pass test to have a player pick up a stick

---
+++ sticks.py
@@ -0,0 +1,23 @@
+player1 = "Maple"
+player2 = "Danai"
+
+stick_count = 20
+def make_move(player, sticks):
+    return stick_count - sticks
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
