42f568df88fc0851d9281d5808c0733d94259179
Danai Adkisson <dadkisso@live.com>
2015-09-08 14:13:20 -0400

    player vs player function will take 1 players move and properly calculate it

--- sticks.py
+++ sticks.py
@@ -1,15 +1,25 @@
-player1 = "Maple"
+
 player2 = "Danai"
 
-stick_count = 20
-def make_move(player, sticks):
-    return stick_count - sticks
+
+def make_move(stick_count, pick_up):
+    return stick_count - int(pick_up)
 
 def game_complete(stick_count):
     complete = False
     if stick_count < 1:
         complete = True
     return complete
+
+def player_vs_player():
+    player1 = "Maple"
+    stick_count = 20
+    print("There are {} sticks in play.\n".format(stick_count))
+    pick_up = input("""{}, it is your turn. How many sticks will you pick up:
+        [1], [2] or [3]""".format(player1))
+    stick_count = make_move(stick_count , pick_up)
+    print(stick_count)
+
 
 
 
@@ -26,4 +36,4 @@ def game_complete(stick_count):
 
 
 if __name__ == '__main__':
-    main()
+    player_vs_player()
